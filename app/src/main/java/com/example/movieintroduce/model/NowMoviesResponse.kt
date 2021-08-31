9fb339d096fa6db406ef42d8bf16dd5db02c8872
sm1374 <36175704+idstudent@users.noreply.github.com>
2021-08-31 13:45:40 +0900

    Revert "안쓰는 클래스삭제"
    This reverts commit 50cba21026227bbe2d15eaa5811bbe91b1f1cc58.

---
+++ app/src/main/java/com/example/movieintroduce/model/NowMoviesResponse.kt
@@ -0,0 +1,31 @@
+package com.example.movieintroduce.model
+
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class NowMoviesResponseT (
+    @SerializedName("page")
+    var page : Int,
+    @SerializedName("results")
+    var movieInfoList : ArrayList<MovieInfo>
+)
+data class MovieInfo(
+    @SerializedName("id")
+    var movieId : Int,
+    @SerializedName("backdrop_path") // 상세 이미지
+    var detailImg : String,
+//    @SerializedName("genre_ids") // 장르
+//    var genre : ArrayList<Int>,
+    @SerializedName("overview") // 영화소개
+    var movieOverView : String,
+    @SerializedName("poster_path") // 영화 포스터이미지
+    var movieMainImg : String,
+    @SerializedName("release_date") // 개봉일
+    var releaseDate : String,
+    @SerializedName("title") // 영화제목
+    var movieTitle : String,
+    @SerializedName("vote_average") // 평점
+    var movieGrade : Double
+):Serializable
